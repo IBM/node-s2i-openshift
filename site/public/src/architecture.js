87f18a149fbd770020a5c951c03e226e6e955941
Anton McConville <anton@hickory.ca>
2019-02-28 14:57:39 -0500

    labels

--- site/public/src/architecture.js
+++ site/public/src/architecture.js
@@ -6,48 +6,49 @@ if (c != undefined) {
   ctx.stroke();
   ctx.lineWidth = 0.5;
 
-  drawzOS()
-  drawContainer()
-  drawNodeApp();
+  drawzOS("z/OS")
+  drawContainer("IBM Container Service")
+  drawNodeApp("CF - ICP");
 }
 
-function drawComponent(x, y){
+function drawComponent(x, y, label){
   var height = 60;
   var width = 160;
   ctx.fillStyle = "#99DDE5"
   ctx.fillRect(x,y,width,height);
   ctx.strokeRect(x,y,width,height);
-}
-
-function drawNodeApp(){
-  drawSubsystem(5,50,200,120);
-  drawComponent(25,90);
-
   ctx.fillStyle = "#0F4C81"
-  ctx.font = '12px sans-serif';
-  ctx.fillText("CF - ICP", 25, 75);
-  ctx.fillText("Patient UI - Node JS", 48, 125);
+  ctx.font = 'bold 11px sans-serif';
+  ctx.fillText(label, x+10, y+ 35);
 }
 
-function drawContainer(){
-  drawSubsystem(355,50,200,360);
-  drawComponent(375,90);
-  drawComponent(375,170);
-  drawComponent(375,250);
-  drawComponent(375,330);
+function drawNodeApp(label){
+  drawSubsystem(5,50,200,120, label);
+  drawComponent(25,90, "Patient UI - Node JS");
 }
 
-function drawSubsystem(x,y,width,height){
+function drawContainer(label){
+  drawSubsystem(355,50,200,360, label);
+  drawComponent(375,90, "Analytics UI - Node JS");
+  drawComponent(375,170, "Analytics App - Node JS");
+  drawComponent(375,250, "Analytics API - Node JS");
+  drawComponent(375,330, "Allergy data lake - Mongo");
+}
+
+function drawSubsystem(x,y,width,height, label){
   ctx.fillStyle = "#CCEEF2";
   ctx.strokeStyle = "#00ABC0"
   ctx.fillRect(x,y,width,height);
-  ctx.strokeRect(5,200,200,360);
+  ctx.strokeRect(x,y,width,height);
+  ctx.fillStyle = "#0F4C81"
+  ctx.font = 'bold 11px sans-serif';
+  ctx.fillText(label, x+20, y+25);
 }
 
-function drawzOS(){
-  drawSubsystem(5,200,200,360);
-  drawComponent(25,240);
-  drawComponent(25,320);
-  drawComponent(25,400);
-  drawComponent(25,480);
+function drawzOS(label){
+  drawSubsystem(5,200,200,360, label);
+  drawComponent(25,240, "z/OS Connect");
+  drawComponent(25,320, "Cobol processing");
+  drawComponent(25,400, "Machine Learning - Python");
+  drawComponent(25,480, "Patient Records - DB2");
 }
