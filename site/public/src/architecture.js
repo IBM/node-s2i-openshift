7b308b6162b0288c07ec4ad0db463cb5b0094c35
Anton McConville <anton@hickory.ca>
2019-02-28 12:08:47 -0500

    more architecture stuff

--- site/public/src/architecture.js
+++ site/public/src/architecture.js
@@ -1,36 +1,53 @@
 var c = document.getElementById("canvas");
 
 if (c != undefined) {
-
   var ctx = c.getContext("2d");
-  ctx.fillStyle = "#CCEEF2";
-  ctx.strokeStyle = "#00ABC0"
   ctx.beginPath();
   ctx.stroke();
-  ctx.lineWidth = 0.8;
+  ctx.lineWidth = 0.5;
 
-  ctx.fillRect(5,50,200,120);
-  ctx.strokeRect(5,50,200,120);
+  drawzOS()
+  drawContainer()
+  drawNodeApp();
+}
 
+function drawComponent(x, y){
+  var height = 60;
+  var width = 160;
   ctx.fillStyle = "#99DDE5"
-  ctx.fillRect(25,90,160,60);
-  ctx.strokeRect(25,90,160,60);
-  ctx.fillStyle = "#CCEEF2";
-  ctx.fillRect(355,50,200,120);
-  ctx.strokeRect(355,50,200,120);
+  ctx.fillRect(x,y,width,height);
+  ctx.strokeRect(x,y,width,height);
+}
 
-  ctx.fillStyle = "#99DDE5"
-  ctx.fillRect(375,90,160,60);
-  ctx.strokeRect(375,90,160,60);
+function drawNodeApp(){
+  drawSubsystem(5,50,200,120);
+  drawComponent(25,90);
 
   ctx.fillStyle = "#0F4C81"
-
   ctx.font = '12px sans-serif';
-
   ctx.fillText("CF - ICP", 25, 75);
-
   ctx.fillText("Patient UI - Node JS", 48, 125);
+}
 
+function drawContainer(){
+  drawSubsystem(355,50,200,360);
+  drawComponent(375,90);
+  drawComponent(375,170);
+  drawComponent(375,250);
+  drawComponent(375,330);
+}
 
+function drawSubsystem(x,y,width,height){
+  ctx.fillStyle = "#CCEEF2";
+  ctx.strokeStyle = "#00ABC0"
+  ctx.fillRect(x,y,width,height);
+  ctx.strokeRect(5,200,200,360);
+}
 
+function drawzOS(){
+  drawSubsystem(5,200,200,360);
+  drawComponent(25,240);
+  drawComponent(25,320);
+  drawComponent(25,400);
+  drawComponent(25,480);
 }
