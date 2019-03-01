6b4184bf2ff7cce25ef5dc4735ab7bded977833e
Anton McConville <anton@hickory.ca>
2019-03-01 10:24:18 -0500

    More architecture

--- site/public/src/architecture.js
+++ site/public/src/architecture.js
@@ -4,11 +4,39 @@ if (c != undefined) {
   var ctx = c.getContext("2d");
   ctx.beginPath();
   ctx.stroke();
-  ctx.lineWidth = 0.5;
+  ctx.lineWidth = 0.8;
 
   drawzOS("z/OS")
   drawContainer("IBM Container Service")
   drawNodeApp("CF - ICP");
+  drawIsland(5, 590, 200, 50, "Synthea - Data Generation")
+  drawIsland(230, 200, 100, 100, "API Connect")
+
+
+ctx.lineWidth = 1.0;
+  ctx.beginPath();
+  ctx.moveTo(240, 220);
+  ctx.lineTo(170, 140);
+
+  ctx.moveTo(175, 280);
+  ctx.lineTo(240, 280);
+
+  ctx.moveTo(320, 280);
+  ctx.lineTo(385, 350);
+
+  ctx.moveTo(100, 530);
+  ctx.lineTo(100, 605);
+
+  ctx.stroke();
+}
+
+function drawIsland(x,y, width, height, label){
+  ctx.fillStyle = "#99DDE5"
+  ctx.fillRect(x,y,width,height);
+  ctx.strokeRect(x,y,width,height);
+  ctx.fillStyle = "#0F4C81"
+  ctx.font = 'bold 11px sans-serif';
+  ctx.fillText(label, x+10, y+ height/1.8);
 }
 
 function drawComponent(x, y, label){
