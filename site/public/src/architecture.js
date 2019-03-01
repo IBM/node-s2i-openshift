2e6e49a45b2cc895c04cb0645286381d0dc01c1e
Anton McConville <anton@hickory.ca>
2019-03-01 15:35:43 -0500

    beginning integration

--- site/public/src/architecture.js
+++ site/public/src/architecture.js
@@ -1,10 +1,20 @@
 var c = document.getElementById("canvas");
 
+var AQUA = "#00ABC0";
+var NAVY = "#0F4C81";
+var AQUALIGHT = "#99DDE5"
+var EXTRALIGHT = "#CCEEF2"
+
 if (c != undefined) {
   var ctx = c.getContext("2d");
   ctx.beginPath();
   ctx.stroke();
   ctx.lineWidth = 0.8;
+
+  ctx.fillStyle = "#0F4C81"
+  ctx.font = 'bold 12px sans-serif';
+  ctx.fillText("Patient System", 5, 20);
+  ctx.fillText("Analytics System", 455, 20);
 
   drawzOS("z/OS")
   drawContainer("IBM Container Service")
@@ -12,8 +22,7 @@ if (c != undefined) {
   drawIsland(5, 590, 200, 50, "Synthea - Data Generation")
   drawIsland(230, 200, 100, 100, "API Connect")
 
-
-ctx.lineWidth = 1.0;
+  ctx.lineWidth = 1.0;
   ctx.beginPath();
   ctx.moveTo(240, 220);
   ctx.lineTo(170, 140);
@@ -28,55 +37,121 @@ ctx.lineWidth = 1.0;
   ctx.lineTo(100, 605);
 
   ctx.stroke();
+
+  c.onmousemove = function(e) {
+    // important: correct mouse position:
+    var rect = this.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top,
+      i = 0,
+      r;
+
+    // console.log("x: " + x + " y: " + y)
+
+    /* node app - 5,50,200,120 */
+
+    var element = document.getElementById("archtitle");
+    element.innerHTML = ""
+
+    if (x > 5 && x < 205) {
+      if (y > 50 && y < 170) {
+        console.log('node app')
+
+
+        element.innerHTML = "Writing a Modern Web UI using existing System Z application"
+
+      }
+    }
+
+    if (x > 355 && x < 555) {
+      if (y > 50 && y < 410) {
+        console.log('node app')
+        element.innerHTML = "Creating a health data analytics/ML app that integrates with historic data"
+      }
+    }
+
+
+    // ctx.clearRect(0, 0, canvas.width, canvas.height); // for demo
+
+    // while(r = rects[i++]) {
+    //   // add a single rect to path:
+    //   ctx.beginPath();
+    //   ctx.rect(r.x, r.y, r.w, r.h);
+    //
+    //   // check if we hover it, fill red, if not fill it blue
+    //   ctx.fillStyle = ctx.isPointInPath(x, y) ? "red" : "blue";
+    //   ctx.fill();
+    // }
+  }
+
 }
 
-function drawIsland(x,y, width, height, label){
-  ctx.fillStyle = "#99DDE5"
-  ctx.fillRect(x,y,width,height);
-  ctx.strokeRect(x,y,width,height);
-  ctx.fillStyle = "#0F4C81"
+function fillStripes(context, x, y, width, height) {
+  var color1 = "#FFFFFF",
+    color2 = "#CCEEF2";
+  var numberOfStripes = 80;
+  for (var i = 0; i < numberOfStripes * 2; i++) {
+    var thickness = width / numberOfStripes;
+    context.beginPath();
+    context.strokeStyle = i % 2 ? color1 : color2;
+    context.lineWidth = thickness;
+    context.lineCap = 'round';
+    context.moveTo(x + i * thickness + thickness / 2 - width, y);
+    context.lineTo(x + i * thickness + thickness / 2, y+ height);
+    context.stroke();
+  }
+}
+
+function drawIsland(x, y, width, height, label) {
+  ctx.fillStyle = AQUALIGHT
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeRect(x, y, width, height);
+  ctx.fillStyle = NAVY
   ctx.font = 'bold 11px sans-serif';
-  ctx.fillText(label, x+10, y+ height/1.8);
+  ctx.fillText(label, x + 10, y + height / 1.8);
 }
 
-function drawComponent(x, y, label){
+function drawComponent(x, y, label) {
   var height = 60;
   var width = 160;
-  ctx.fillStyle = "#99DDE5"
-  ctx.fillRect(x,y,width,height);
-  ctx.strokeRect(x,y,width,height);
+  ctx.fillStyle = AQUALIGHT
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeRect(x, y, width, height);
+  ctx.fillStyle = NAVY
+  ctx.font = 'bold 11px sans-serif';
+  ctx.fillText(label, x + 10, y + 35);
+}
+
+function drawNodeApp(label) {
+  drawSubsystem(5, 50, 200, 120, label);
+  drawComponent(25, 90, "Patient UI - Node JS");
+}
+
+function drawContainer(label) {
+  drawSubsystem(355, 50, 200, 360, label);
+  drawComponent(375, 90, "Analytics UI - Node JS");
+  drawComponent(375, 170, "Analytics App - Node JS");
+  drawComponent(375, 250, "Analytics API - Node JS");
+  drawComponent(375, 330, "Allergy data lake - Mongo");
+}
+
+function drawSubsystem(x, y, width, height, label) {
+  ctx.fillStyle = EXTRALIGHT;
+  ctx.strokeStyle = AQUA
+  ctx.fillRect(x, y, width, height);
+
+  // fillStripes(ctx, x, y, width, height);
+  ctx.strokeStyle = AQUA
+  ctx.strokeRect(x, y, width, height);
   ctx.fillStyle = "#0F4C81"
   ctx.font = 'bold 11px sans-serif';
-  ctx.fillText(label, x+10, y+ 35);
+  ctx.fillText(label, x + 20, y + 25);
 }
 
-function drawNodeApp(label){
-  drawSubsystem(5,50,200,120, label);
-  drawComponent(25,90, "Patient UI - Node JS");
-}
-
-function drawContainer(label){
-  drawSubsystem(355,50,200,360, label);
-  drawComponent(375,90, "Analytics UI - Node JS");
-  drawComponent(375,170, "Analytics App - Node JS");
-  drawComponent(375,250, "Analytics API - Node JS");
-  drawComponent(375,330, "Allergy data lake - Mongo");
-}
-
-function drawSubsystem(x,y,width,height, label){
-  ctx.fillStyle = "#CCEEF2";
-  ctx.strokeStyle = "#00ABC0"
-  ctx.fillRect(x,y,width,height);
-  ctx.strokeRect(x,y,width,height);
-  ctx.fillStyle = "#0F4C81"
-  ctx.font = 'bold 11px sans-serif';
-  ctx.fillText(label, x+20, y+25);
-}
-
-function drawzOS(label){
-  drawSubsystem(5,200,200,360, label);
-  drawComponent(25,240, "z/OS Connect");
-  drawComponent(25,320, "Cobol processing");
-  drawComponent(25,400, "Machine Learning - Python");
-  drawComponent(25,480, "Patient Records - DB2");
+function drawzOS(label) {
+  drawSubsystem(5, 200, 200, 360, label);
+  drawComponent(25, 240, "z/OS Connect");
+  drawComponent(25, 320, "Cobol processing");
+  drawComponent(25, 400, "Machine Learning - Python");
+  drawComponent(25, 480, "Patient Records - DB2");
 }
