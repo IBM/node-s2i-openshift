7ab0494b3ffa8d627728eb7c27ffe0b56223fa5b
Anton McConville <anton@hickory.ca>
2019-03-07 22:36:12 -0500

    more interactive diagram

--- site/public/src/architecture.js
+++ site/public/src/architecture.js
@@ -1,28 +1,47 @@
+if (!sessionStorage.getItem("patientid")) {
+  console.log("Redirecting to login");
+  window.location = '/login.html';
+} else {
+  var patientlogout = document.getElementById('logout');
+  patientlogout.innerHTML = sessionStorage.getItem("patientusername") + "/logout";
+}
+
 var c = document.getElementById("canvas");
 
 var AQUA = "#00ABC0";
 var NAVY = "#0F4C81";
-var AQUALIGHT = "#99DDE5"
-var EXTRALIGHT = "#CCEEF2"
+var AQUALIGHT = "#99DDE5";
+var EXTRALIGHT = "#CCEEF2";
+var HIGHLIGHT = "#F88F58";
+var HIGHFILL = "#fcddcc";
+var HIGHCOMPONENTFILL = "#fab08a";
+
+var LOWFILL = "#e5f6f8";
+var LOWSTROKE = "#cceef2";
+var LOWFONT = "#99dde5";
+
+var HIGHLIGHTED = 0;
+var DIMMED = 1;
+var NORMAL = 2;
 
 if (c != undefined) {
   var ctx = c.getContext("2d");
   ctx.beginPath();
   ctx.stroke();
-  ctx.lineWidth = 0.8;
+  ctx.lineWidth = 1.5;
 
-  ctx.fillStyle = "#0F4C81"
+  ctx.fillStyle = "#0F4C81";
   ctx.font = 'bold 12px sans-serif';
   ctx.fillText("Patient System", 5, 20);
   ctx.fillText("Analytics System", 455, 20);
 
-  drawzOS("z/OS")
-  drawContainer("IBM Container Service")
+  drawzOS("z/OS");
+  drawContainer("IBM Container Service");
   drawNodeApp("CF - ICP");
-  drawIsland(5, 590, 200, 50, "Synthea - Data Generation")
-  drawIsland(230, 200, 100, 100, "API Connect")
+  drawIsland(5, 590, 200, 50, "Synthea - Data Generation", NORMAL);
+  drawIsland(230, 200, 100, 100, "API Connect", NORMAL);
 
-  ctx.lineWidth = 1.0;
+  ctx.lineWidth = 1.5;
   ctx.beginPath();
   ctx.moveTo(240, 220);
   ctx.lineTo(170, 140);
@@ -46,31 +65,60 @@ if (c != undefined) {
       i = 0,
       r;
 
-    // console.log("x: " + x + " y: " + y)
-
-    /* node app - 5,50,200,120 */
-
     var element = document.getElementById("archtitle");
-    element.innerHTML = ""
+    // element.innerHTML = ""
 
-    if (x > 5 && x < 205) {
-      if (y > 50 && y < 170) {
-        console.log('node app')
-        displayInfo(modernAppInfo)
-      }
-    }else{
+    var general = true;
 
-      if (x > 355 && x < 555) {
-        if (y > 50 && y < 410) {
-          console.log('analytics app')
-          displayInfo(analyticsInfo)
-        }
-      }else{
-          displayInfo(generalInfo)
-      }
+    if (x > 5 && x < 205 && y > 50 && y < 170) {
+      console.log('node app');
+      displayInfo(modernAppInfo);
+      drawNodeApp("CF - ICP", HIGHLIGHTED);
+      drawIsland(230, 200, 100, 100, "API Connect", HIGHLIGHTED);
+      drawzOS("z/OS", DIMMED);
+      drawContainer("IBM Container Service", DIMMED);
+      drawComponent(25, 240, "z/OS Connect", HIGHLIGHTED);
+      drawComponent(25, 480, "Patient Records - DB2", HIGHLIGHTED);
+      drawComponent(25, 320, "Cobol processing", HIGHLIGHTED);
+      drawIsland(5, 590, 200, 50, "Synthea - Data Generation", DIMMED);
+
+      general = false;
     }
 
+    if (x > 355 && x < 555 && y > 50 && y < 410) {
+      console.log('analytics app');
+      drawContainer("IBM Container Service", HIGHLIGHTED);
+      drawNodeApp("CF - ICP", DIMMED);
+      drawIsland(230, 200, 100, 100, "API Connect", HIGHLIGHTED);
+      drawzOS("z/OS", DIMMED);
+      drawComponent(25, 240, "z/OS Connect", HIGHLIGHTED);
+      drawComponent(25, 480, "Patient Records - DB2", HIGHLIGHTED);
+      drawComponent(25, 320, "Cobol processing", HIGHLIGHTED);
+            drawIsland(5, 590, 200, 50, "Synthea - Data Generation", DIMMED);
 
+      displayInfo(analyticsInfo);
+      general = false;
+    }
+
+    if (x > 5 && x < 205 && y > 590 && y < 630) {
+        drawIsland(5, 590, 200, 50, "Synthea - Data Generation", HIGHLIGHTED);
+        drawNodeApp("CF - ICP", DIMMED);
+        drawIsland(230, 200, 100, 100, "API Connect", DIMMED);
+        drawzOS("z/OS", DIMMED);
+        drawContainer("IBM Container Service", DIMMED);
+        drawComponent(25, 480, "Patient Records - DB2", HIGHLIGHTED);
+        drawIsland(230, 200, 100, 100, "API Connect", DIMMED);
+        general = false;
+    }
+
+    if (general === true) {
+      displayInfo(generalInfo)
+      drawNodeApp("CF - ICP", NORMAL);
+      drawIsland(230, 200, 100, 100, "API Connect", NORMAL);
+      drawzOS("z/OS", NORMAL);
+      drawContainer("IBM Container Service", NORMAL);
+      drawIsland(5, 590, 200, 50, "Synthea - Data Generation", NORMAL);
+    }
 
     // ctx.clearRect(0, 0, canvas.width, canvas.height); // for demo
 
@@ -88,7 +136,7 @@ if (c != undefined) {
 }
 
 
-function displayInfo(info){
+function displayInfo(info) {
 
   var title = document.getElementById("archtitle");
   title.innerHTML = info.title;
@@ -102,7 +150,7 @@ function displayInfo(info){
   var techlist = document.getElementById("techlist");
   techlist.innerHTML = ""
 
-  info.technologies.forEach(function(technology){
+  info.technologies.forEach(function(technology) {
 
     var li = document.createElement('li');
     li.innerHTML = technology;
@@ -126,84 +174,164 @@ function fillStripes(context, x, y, width, height) {
     context.lineWidth = thickness;
     context.lineCap = 'round';
     context.moveTo(x + i * thickness + thickness / 2 - width, y);
-    context.lineTo(x + i * thickness + thickness / 2, y+ height);
+    context.lineTo(x + i * thickness + thickness / 2, y + height);
     context.stroke();
   }
 }
 
-function drawIsland(x, y, width, height, label) {
-  ctx.fillStyle = AQUALIGHT
+function drawIsland(x, y, width, height, label, state) {
+
+  switch (state) {
+
+    case HIGHLIGHTED:
+      ctx.fillStyle = HIGHCOMPONENTFILL;
+      ctx.strokeStyle = HIGHLIGHT
+
+      break;
+
+    case NORMAL:
+      ctx.fillStyle = AQUALIGHT;
+      break;
+
+    case DIMMED:
+      ctx.fillStyle = LOWFILL;
+      ctx.strokeStyle = LOWSTROKE;
+      break;
+
+    default:
+      ctx.fillStyle = AQUALIGHT;
+      break;
+  }
+
   ctx.fillRect(x, y, width, height);
   ctx.strokeRect(x, y, width, height);
-  ctx.fillStyle = NAVY
+  if (state === DIMMED) {
+    ctx.fillStyle = LOWFONT
+  } else {
+    ctx.fillStyle = NAVY
+  }
   ctx.font = 'bold 11px sans-serif';
   ctx.fillText(label, x + 10, y + height / 1.8);
 }
 
-function drawComponent(x, y, label) {
+function drawComponent(x, y, label, state) {
   var height = 60;
   var width = 160;
-  ctx.fillStyle = AQUALIGHT
+
+  switch (state) {
+
+    case HIGHLIGHTED:
+      ctx.fillStyle = HIGHCOMPONENTFILL;
+      ctx.strokeStyle = HIGHLIGHT;
+      break;
+
+    case NORMAL:
+      ctx.fillStyle = AQUALIGHT;
+      break;
+
+    case DIMMED:
+      ctx.fillStyle = LOWFILL;
+      break;
+
+    default:
+      ctx.fillStyle = AQUALIGHT;
+      break;
+  }
+
   ctx.fillRect(x, y, width, height);
   ctx.strokeRect(x, y, width, height);
-  ctx.fillStyle = NAVY
+  if (state === DIMMED) {
+    ctx.fillStyle = LOWFONT
+  } else {
+    ctx.fillStyle = NAVY
+  }
   ctx.font = 'bold 11px sans-serif';
   ctx.fillText(label, x + 10, y + 35);
 }
 
-function drawNodeApp(label) {
-  drawSubsystem(5, 50, 200, 120, label);
-  drawComponent(25, 90, "Patient UI - Node JS");
+function drawNodeApp(label, state) {
+  drawSubsystem(5, 50, 200, 120, label, state);
+  drawComponent(25, 90, "Patient UI - Node JS", state);
 }
 
-function drawContainer(label) {
-  drawSubsystem(355, 50, 200, 360, label);
-  drawComponent(375, 90, "Analytics UI - Node JS");
-  drawComponent(375, 170, "Analytics App - Node JS");
-  drawComponent(375, 250, "Analytics API - Node JS");
-  drawComponent(375, 330, "Allergy data lake - Mongo");
+function drawContainer(label, state) {
+  drawSubsystem(355, 50, 200, 360, label, state);
+  drawComponent(375, 90, "Analytics UI - Node JS", state);
+  drawComponent(375, 170, "Analytics App - Node JS", state);
+  drawComponent(375, 250, "Analytics API - Node JS", state);
+  drawComponent(375, 330, "Allergy data lake - Mongo", state);
 }
 
-function drawSubsystem(x, y, width, height, label) {
-  ctx.fillStyle = EXTRALIGHT;
-  ctx.strokeStyle = AQUA
+function drawSubsystem(x, y, width, height, label, state) {
+
+  switch (state) {
+
+    case HIGHLIGHTED:
+      ctx.fillStyle = HIGHFILL;
+      ctx.strokeStyle = HIGHLIGHT
+      break;
+
+    case NORMAL:
+      ctx.fillStyle = EXTRALIGHT;
+      ctx.strokeStyle = AQUA;
+      break;
+
+    case DIMMED:
+      ctx.fillStyle = LOWFILL;
+      ctx.strokeStyle = LOWSTROKE;
+      break;
+
+    default:
+      ctx.fillStyle = EXTRALIGHT;
+      ctx.strokeStyle = AQUA
+      break;
+  }
+
   ctx.fillRect(x, y, width, height);
-
   // fillStripes(ctx, x, y, width, height);
-  ctx.strokeStyle = AQUA
   ctx.strokeRect(x, y, width, height);
-  ctx.fillStyle = "#0F4C81"
+
+  if (state === DIMMED) {
+    ctx.fillStyle = LOWFONT
+  } else {
+    ctx.fillStyle = NAVY
+  }
+
   ctx.font = 'bold 11px sans-serif';
   ctx.fillText(label, x + 20, y + 25);
 }
 
-function drawzOS(label) {
-  drawSubsystem(5, 200, 200, 360, label);
-  drawComponent(25, 240, "z/OS Connect");
-  drawComponent(25, 320, "Cobol processing");
-  drawComponent(25, 400, "Machine Learning - Python");
-  drawComponent(25, 480, "Patient Records - DB2");
+function drawzOS(label, state) {
+  drawSubsystem(5, 200, 200, 360, label, state);
+  drawComponent(25, 240, "z/OS Connect", state);
+  drawComponent(25, 320, "Cobol processing", state);
+  drawComponent(25, 400, "Machine Learning - Python", state);
+  drawComponent(25, 480, "Patient Records - DB2", state);
 }
 
 var generalInfo = {
-  title:"Summit Health Software Architecture",
-  subtitle:"An Open Source Case Study",
-  description:"Summit Health is an experimental project, and open source reference architecture for integrating a legacy data system, with modern cloud technology.",
-  technologies:["IBM zSystems","IBM Cloud Container Service","IBM Cloud Private","IBM Watson Data Platform","IBM API Connect"],
-  pattern:""
+  title: "Summit Health Software Architecture",
+  subtitle: "An Open Source Case Study",
+  description: "Summit Health is an experimental project, and open source reference architecture for integrating a legacy data system, with modern cloud technology.",
+  technologies: ["IBM zSystems", "IBM Cloud Container Service", "IBM Cloud Private", "IBM Watson Data Platform", "IBM API Connect"],
+  pattern: ""
 }
 
-var modernAppInfo = {  title:"Writing a Modern Web UI using existing System Z application",
-  subtitle:"Agile UI development",
-  description:"In this pattern, we show how to rapidly prototype a new web UI built on Node JS, using HTML5 technology, surfacing legacy data in fresh ways. ",
-  technologies:["IBM zSystems","IBM Cloud Private","IBM API Connect"],
-  pattern:""}
+var modernAppInfo = {
+  title: "Writing a Modern Web UI using existing System Z application",
+  subtitle: "Agile UI development",
+  description: "In this pattern, we show how to rapidly prototype a new web UI built on Node JS, using HTML5 technology, surfacing legacy data in fresh ways. ",
+  technologies: ["IBM zSystems", "IBM Cloud Private", "IBM API Connect"],
+  pattern: ""
+}
 
-var analyticsInfo = {  title:"A health data analytics app that integrates with historic data",
-  subtitle:"Creating a full stack big data app",
-  description:"In this pattern, we build a full stack containerized application that delves into big data, using Node JS and the Watson Data Platform.",
-  technologies:["IBM zSystems","IBM Cloud Container Service","IBM API Connect"],
-  pattern:""}
+var analyticsInfo = {
+  title: "A health data analytics app that integrates with historic data",
+  subtitle: "Creating a full stack big data app",
+  description: "In this pattern, we build a full stack containerized application that delves into big data, using Node JS and the Watson Data Platform.",
+  technologies: ["IBM zSystems", "IBM Cloud Container Service", "IBM API Connect"],
+  pattern: ""
+}
 
 var machinelearning = {}
 
